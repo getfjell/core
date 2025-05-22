063640fbec458aed79eaead373519deb08a6093c
Tim O'Brien <tobrien@discursive.com>
2025-05-22 12:09:45 -0700

    Fixed a test

--- tests/dictionary.test.ts
+++ tests/dictionary.test.ts
@@ -1,5 +1,5 @@
 import { Dictionary } from "@/dictionary";
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 
 describe('Dictionary', () => {
   let dictionary: Dictionary<string, number>;
