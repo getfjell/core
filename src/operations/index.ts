e257bec7c27fba1633dee3448cd3d01a0e11c1c0
GetFjell <info@getfjell.com>
2025-12-20 07:15:23 -0600

    feat: re-export core operation types from @fjell/types

--- src/operations/index.ts
+++ src/operations/index.ts
@@ -1,3 +1,12 @@
 export * from './OperationContext';
 export * from './errorEnhancer';
 export * from './wrappers';
+
+// Re-export core operation types for convenience
+export type {
+  UpdateOptions,
+  CreateOptions,
+  FindOptions,
+  AllOptions,
+  PaginationOptions
+} from '@fjell/types';
